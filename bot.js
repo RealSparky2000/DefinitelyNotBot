7953b2b48e5dff7ccdf4e02fb449cb5369352bb0
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-21 01:16:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
-  	}
-});
-
-client.on('message', message => {
+  }
+        
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
 });
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(BOT_TOKEN);
